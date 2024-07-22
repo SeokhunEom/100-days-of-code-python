66b7e04e6c2a33340162c45b6a3623ae34ca7509
SeokhunEom <tommya98@naver.com>
2024-07-22 13:22:13 +0900

    day21 추가

---
+++ day21/snake.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+
+STARTING_X = 0
+STARTING_Y = 0
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in range(3):
+            new_position = (STARTING_X - MOVE_DISTANCE * i, STARTING_Y)
+            self.add_segment(new_position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for index in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[index - 1].xcor()
+            new_y = self.segments[index - 1].ycor()
+            self.segments[index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
